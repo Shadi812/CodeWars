151d73b09456beb52a8857cce4133d463b963b48
Shadi <shadihaggag@gmail.com>
2023-06-11 18:41:40 -0400

    added comments

--- 8-kyu/Removing-Elements.js
+++ 8-kyu/Removing-Elements.js
@@ -7,9 +7,14 @@
 // None of the arrays will be empty, so you don't have to worry about that!
 
 function removeEveryOther(arr) {
+  // Create a new array with the first element from the input array
   const newArray = [arr[0]];
+  // Iterate through the input array, starting from index 2 and incrementing by 2
   for (let i = 2; i < arr.length; i += 2) {
+    // Add the element at the current index to the new array
     newArray.push(arr[i]);
   }
+  // Return the new array
+
   return newArray;
 }
